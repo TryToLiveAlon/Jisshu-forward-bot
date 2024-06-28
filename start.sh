609aaac78a85192cef817bdb2b8046afea97b5f0
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-06-28 11:56:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/TryToLiveAlon/Jisshu-forward-bot SadKidBGMZ/fwdbot 
+  git clone https://github.com/TryToLiveAlon/Jisshu-forward-bot TryToLiveAlon/fwdbot 
 else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/TryToLiveAlon/Jisshu-forward-bot -b $BRANCH /fwdbot
