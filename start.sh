cfc02b48d4085d0bfb99854240f3f01fc6921574
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-06-28 11:55:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot SadKidBGMZ/fwdbot 
+  git clone https://github.com/TryToLiveAlon/Jisshu-forward-bot SadKidBGMZ/fwdbot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot -b $BRANCH /fwdbot
+  git clone https://github.com/TryToLiveAlon/Jisshu-forward-bot -b $BRANCH /fwdbot
 fi
 cd SadKidBGMZ/fwdbot 
 pip3 install -U -r requirements.txt
