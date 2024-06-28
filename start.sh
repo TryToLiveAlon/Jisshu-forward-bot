cad553fc092839709a43cb369d944424055a14e5
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-06-28 11:59:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/TryToLiveAlon/Jisshu-forward-bot -b $BRANCH /fwdbot
 fi
-cd SadKidBGMZ/fwdbot 
+cd TryToLiveAlon/fwdbot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
